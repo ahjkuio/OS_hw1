4f60583d06f6db5a202567dfbe0b6160780c0357
Kuznetsov Artem <st085972@student.spbu.ru>
2025-03-14 18:07:04 +0300

    Улучшение task2: оптимизация чтения из pipe и обработка ошибок
    - Заменено побайтовое чтение из pipe на чтение блоками с размером буфера 1024 байта
    - Добавлены проверки на ошибки при закрытии канала для обоих концов pipe
    - Улучшена обработка ошибок во всех системных вызовах
    - Добавлены информативные сообщения об ошибках для удобства отладки

--- user/task2.c
+++ user/task2.c
@@ -2,58 +2,81 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+#define BUFFER_SIZE 1024  // Размер буфера для чтения блоками
+
 int
 main(int argc, char *argv[])
 {
-  int p[2];
+  int pipefd[2];
+  int pid;
+  int ret;
   
-  if(pipe(p) < 0){
-    fprintf(2, "pipe error!\n");
+  if(pipe(pipefd) < 0){
+    fprintf(2, "task2: ошибка создания pipe\n");
     exit(1);
   }
 
-  int pid = fork();
+  pid = fork();
   
-  if(pid > 0){ // родительский процесс
-    close(p[0]);
-    
-    // записываем все аргументы, начиная с первого
-    for(int i = 1; i < argc; i++){
-      int res = write(p[1], argv[i], strlen(argv[i]));
-      if(res < 0){
-        fprintf(2, "write error!\n");
-        break;
-      }
-      
-      res = write(p[1], "\n", 1);
-      if(res < 0){
-        fprintf(2, "write error!\n");
-        break;
-      }
-    }
-    
-    close(p[1]);
-    wait(0);
-    
-  } else if(pid == 0){ // дочерний процесс
-    close(p[1]);
-    
-    char c;
-    int res = read(p[0], &c, 1);
-    while(res > 0){
-      write(1, &c, 1);
-      res = read(p[0], &c, 1);
-    }
-    
-    if(res < 0) 
-      fprintf(2, "read error!\n");
-    
-    close(p[0]);
-    exit(0);
-    
-  } else {
-    fprintf(2, "fork error!\n");
+  if(pid < 0){
+    fprintf(2, "task2: ошибка fork\n");
+    exit(1);
   }
 
-  exit(0);
+  if(pid == 0){
+    char buffer[BUFFER_SIZE];
+    int n;
+
+    if (close(pipefd[1]) < 0) {
+      fprintf(2, "task2: ошибка закрытия дескриптора записи pipe в дочернем процессе\n");
+      exit(1);
+    }
+
+    while ((n = read(pipefd[0], buffer, BUFFER_SIZE)) > 0) {
+      if (write(1, buffer, n) != n) {
+        fprintf(2, "task2: ошибка записи в stdout\n");
+        exit(1);
+      }
+    }
+
+    if (n < 0) {
+      fprintf(2, "task2: ошибка чтения из pipe\n");
+      exit(1);
+    }
+
+    if (close(pipefd[0]) < 0) {
+      fprintf(2, "task2: ошибка закрытия дескриптора чтения pipe в дочернем процессе\n");
+      exit(1);
+    }
+
+    exit(0);
+  } else {
+    if (close(pipefd[0]) < 0) {
+      fprintf(2, "task2: ошибка закрытия дескриптора чтения pipe в родительском процессе\n");
+      exit(1);
+    }
+
+    for(int i = 1; i < argc; i++){
+      if (write(pipefd[1], argv[i], strlen(argv[i])) != strlen(argv[i])) {
+        fprintf(2, "task2: ошибка записи в pipe\n");
+        exit(1);
+      }
+      if (write(pipefd[1], "\n", 1) != 1) {
+        fprintf(2, "task2: ошибка записи символа перевода строки в pipe\n");
+        exit(1);
+      }
+    }
+
+    if (close(pipefd[1]) < 0) {
+      fprintf(2, "task2: ошибка закрытия дескриптора записи pipe в родительском процессе\n");
+      exit(1);
+    }
+
+    if (wait(&ret) != pid) {
+      fprintf(2, "task2: дочерний процесс не завершился корректно\n");
+      exit(1);
+    }
+
+    exit(0);
+  }
 }
